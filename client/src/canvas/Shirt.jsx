97b9593e0b9df33399f21b1eab7dafbbf0646f57
miroj-chamling <mirojrai01@gmail.com>
2023-08-26 09:08:58 +0545

    added logos and enhanced texture, colors

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -16,15 +16,41 @@ const Shirt = () => {
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
 
+  useFrame((state, delta) =>
+    easing.dampC(materials.lambert1.color, snap.color, 0.25, delta)
+  );
+
+  const stateString = JSON.stringify(snap);
+
   return (
-    <group>
+    <group key={stateString}>
       <mesh
         castShadow
         geometry={nodes.T_Shirt_male.geometry}
         material={materials.lambert1}
         material-roughness={1}
         dispose={null}
-      ></mesh>
+      >
+        {snap.isFullTexture && (
+          <Decal
+            position={[0, 0, 0]}
+            rotation={[0, 0, 0]}
+            scale={1}
+            map={fullTexture}
+          />
+        )}
+        {snap.isLogoTexture && (
+          <Decal
+            position={[0.08, 0.05, 0.15]}
+            rotation={[0, 0, 0]}
+            scale={0.1}
+            map={logoTexture}
+            anisotropy={16}
+            depthTest={false}
+            depthWrite={true}
+          />
+        )}
+      </mesh>
     </group>
   );
 };
